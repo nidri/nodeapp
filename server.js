649a93a242974ac90d2ff22ea0a32d9adc3f1f39
Krishna Santosh Nidri <krishna.nidri@gmail.com>
2020-10-03 16:18:21 +0000

    Changed message name

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-	res.send("<h1>Simple Node App</h1>");
+	res.send("<h1>Boilerplate Node App</h1>");
 });
 
 app.listen(9080, () => {});
